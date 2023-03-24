bde141e9233f68446525e9a20033420f5860dbf8
Mahesh Kumar <88311960+kkmk11@users.noreply.github.com>
2023-03-24 22:52:04 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -23,10 +23,10 @@ const Home = ({presentUser}) => {
 
     emailjs
       .sendForm(
-        "service_9qa8bxg",
-        "template_1n5ibqf",
+        "Your service id",
+        "Your template id",
         form.current,
-        "DVJqTGNmaU58-639g"
+        "Your user id"
       )
       .then(
         (result) => {
