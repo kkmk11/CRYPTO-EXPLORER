e6779016bbf305874931c5ae36e275a63db3d588
Mahesh Kumar <88311960+kkmk11@users.noreply.github.com>
2023-03-28 00:05:06 +0530

    Update Signup.js

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -22,10 +22,10 @@ const Signup = () => {
 
     emailjs
       .sendForm(
-        "service_9qa8bxg",
-        "template_1n5ibqf",
+        "service_api",
+        "template_api",
         form.current,
-        "DVJqTGNmaU58-639g"
+        "user_api"
       )
       .then(
         (result) => {
